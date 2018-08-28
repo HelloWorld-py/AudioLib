36aad66d0a51a24da5a102b8b167b64b1ed0ec2b
HelloWorld-py <jdtsekrekos@live.com>
2018-08-28 00:08:12 -0400

    Fixing packaging

--- README.md
+++
@@ -1,3 +0,0 @@
-<h1> PySounds </h1>
-A high level sound playing library for python. This project uses built-in codecs to load a 'waveform', which can then be played back using pyaudio as a backbone.
-<h5>Current version only supports .wav </h5>
--- setup.py
+++ setup.py
@@ -1,10 +1,22 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup(name='pysounds',
-      version='1.0',
+long_description = open("README.md", "r").read()
+
+setup(name='PySounds',
+
+      version='1.0.0b2',
+
       description='A high level sounds api for python',
+
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+
       url='https://github.com/HelloWorld-py/PySounds',
       author='Jacob Tsekrekos',
       author_email='jdtsekrekos@live.com',
-      install_requires=['pyaudio'],
-      packages=['pysounds', 'pysounds/pyaudio'])
+
+      keywords='audio sound media wav player',
+
+      license='MIT',
+      packages=find_packages(),
+      install_requires=['pyaudio'])
